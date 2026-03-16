08121266bd4fec885573c879b11396ff04f0c300
Nazmul Huda <nznazmulhuda04@gmail.com>
2026-03-17 01:53:59 +0600

    chore: dark wrapper layout update

--- src/components/ui/cta/cta.tsx
+++ src/components/ui/cta/cta.tsx
@@ -3,39 +3,41 @@ import Button from '../button/button'
 
 export default function Cta() {
   return (
-    <section className="flex w-full max-w-350 items-center justify-center rounded-xl py-24 pb-4">
+    <section className="w-full px-5 py-24 pb-4">
       <div
         style={{ backgroundImage: `url(${BgImage.src})` }}
-        className="flex w-full items-center justify-center rounded-xl bg-cover py-29.5"
+        className="w-full rounded-xl bg-cover py-29.5"
       >
-        {/* content */}
-        <div className="flex w-full max-w-204 flex-col items-center justify-center">
-          <h2 className="font-jakarta text-center text-[72px] leading-[110%] font-semibold tracking-tight text-white capitalize">
-            Ace your live interviews with AI support!
-          </h2>
+        <div className="flex w-full items-center justify-center">
+          {/* content */}
+          <div className="flex w-full max-w-204 flex-col items-center justify-center">
+            <h2 className="font-jakarta text-center text-[72px] leading-[110%] font-semibold tracking-tight text-white capitalize">
+              Ace your live interviews with AI support!
+            </h2>
 
-          <Button className="mt-10">
-            Get Started For Free{' '}
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 3L11 8L6 13"
-                stroke="black"
-                strokeWidth="1.6"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </Button>
+            <Button className="mt-10">
+              Get Started For Free{' '}
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 3L11 8L6 13"
+                  stroke="black"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </Button>
 
-          <p className="font-inter mt-4 text-lg font-medium text-white">
-            Available on Mac, Windows and iPhone
-          </p>
+            <p className="font-inter mt-4 text-lg font-medium text-white">
+              Available on Mac, Windows and iPhone
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/ui/wrapper/wrapper.dark.tsx
+++ src/components/ui/wrapper/wrapper.dark.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from 'react'
 
 export default function WrapperDark({ children }: { children: ReactNode }) {
   return (
-    <section className="bg-dark-bg flex w-full max-w-350 items-center justify-center rounded-xl py-24">
-      <div className="container">{children}</div>
+    <section className="w-full px-5">
+      <div className="bg-dark-bg flex w-full items-center justify-center rounded-xl py-24">
+        <div className="container w-full">{children}</div>
+      </div>
     </section>
   )
 }
