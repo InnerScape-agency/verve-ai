d793eb527454fc9fd48a61e348afc768af9f819e
Nazmul Huda <nznazmulhuda04@gmail.com>
2026-03-18 01:15:09 +0600

    feat: online assesment copilot page done

--- src/app/(feature)/online-assessment-copilot/page.tsx
+++ src/app/(feature)/online-assessment-copilot/page.tsx
@@ -1,3 +1,35 @@
+import HeroBg from '@/assets/feature-pages/hero/online-assesment-copilot.png'
+import FeatureCodingWorkflow from '@/components/features/feature.codingWorkflow'
+import FeatureInterviewMadeEasy from '@/components/features/feature.interviewMadeEasy'
+import FeatureQuestionBank from '@/components/features/feature.questionBank'
+import FeatureHero from '@/components/features/features.hero'
+import Cta from '@/components/ui/cta/cta'
+import Faq from '@/components/ui/faq/faq'
+import HowItWorks from '@/components/ui/howItWorks/howItWorks'
+
 export default function Page() {
-  return <div>Page</div>
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <FeatureHero
+        badge="Online Assessment"
+        badgeSemiTitle="Premium at best price"
+        heading="Online Assessment Copilot"
+        description="Undetectable plug-in that automatically snap and solve any question on your screen"
+        heroImage={HeroBg}
+        width="max-w-[830px]"
+        imageHeight="h-fit"
+        isClients
+      />
+
+      <HowItWorks />
+
+      <FeatureCodingWorkflow />
+
+      <FeatureInterviewMadeEasy />
+
+      <Faq />
+
+      <Cta varient="withImage" />
+    </section>
+  )
 }
