c087dc8b460f1fc2f0ea0af6a13edb0327a926fa
Nazmul Huda <nazmulhudanahid.dev@gmail.com>
2026-03-20 16:49:17 +0600

    feat: ai cover letter builder page added

--- src/app/(free tools)/ai-cover-letter-builder/page.tsx
+++ src/app/(free tools)/ai-cover-letter-builder/page.tsx
@@ -1,3 +1,37 @@
+import FreeToolsHero from '@/components/freeTools/freeTools.hero'
+import HeroImage from '@/assets/free-tools/hero/ai-cover-latter-builder.png'
+import FeatureInterviewMadeEasy from '@/components/features/feature.interviewMadeEasy'
+import Testimonials from '@/components/ui/testimonials/testimonials'
+import Faq from '@/components/ui/faq/faq'
+import Cta from '@/components/ui/cta/cta'
+import FreeHotTools from '@/components/freeTools/free.hotTools'
+import FreeToolCards from '@/components/freeTools/freeToolCards'
+
 export default function page() {
-  return <div>page</div>
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <FreeToolsHero
+        badgeSemiTitle="AI Cover Letter Builder"
+        heading="Generate Tailored Cover Letters In Seconds"
+        description="Create professional, persuasive cover letters that highlight your skills and match specific job requirements instantly."
+        btnText="Write My Letter Free"
+        btnLink="#"
+        heroImage={HeroImage}
+        width="max-w-200"
+        desWidth="max-w-160"
+      />
+
+      <FreeToolCards />
+
+      <FeatureInterviewMadeEasy varient="dark" />
+
+      <FreeHotTools />
+
+      <Faq />
+
+      <Testimonials />
+
+      <Cta varient="withImage" />
+    </section>
+  )
 }
